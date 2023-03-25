13c41e6de488a9cfb6089384e2c62722be8960da
OverFitted <kozak.bor@gmail.com>
2023-03-25 21:15:59 +0300

    Feat: add filename loading

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,22 +1,39 @@
 package org.example;
 
+import org.example.models.Product;
+
+import java.util.Vector;
+
 public class App {
-    private boolean isWorking;
+    private boolean status;
+    private Vector<Product> products;
+    private String fileName;
 
-    public App() {
-        this.isWorking = true;
-
+    public App(String[] args) {
+        this.status = true;
+        if (args.length == 0) {
+            System.out.println("Пожалуйста, укажите имя файла с данными в качестве аргумента командной строки.");
+            System.exit(1);
+        }
+        fileName = args[0];
+        products = new Vector<>();
+        loadData();
+        status = true;
     }
 
     public void switchOff() {
-        this.isWorking = false;
+        this.status = false;
     }
 
     public Boolean getStatus() {
-        return isWorking;
+        return status;
     }
 
     public void readLine(String line) {
+//        TODO
+    }
+
+    public void loadData() {
 //        TODO
     }
 }
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String args[]) {
-        App app = new App();
+        App app = new App(new String[]{"resources/input.yaml"});
         Scanner keyboard = new Scanner(System.in);
 
         while (app.getStatus()) {
