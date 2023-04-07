47c95cdd3ef6d51fdefc29d03ff0ff06363b15b0
OverFitted <kozak.bor@gmail.com>
2023-04-07 10:55:43 +0300

    Fix: add_if_min command fixed, refactor: add more javadocs

--- src/main/java/exmp/commands/AddCommand.java
+++ src/main/java/exmp/commands/AddCommand.java
@@ -2,19 +2,38 @@ package exmp.commands;
 
 import exmp.models.Product;
 
+/**
+ * Команда add для добавления нового продукта в коллекцию
+ */
 public class AddCommand implements exmp.commands.Command {
     exmp.commands.Utils utils = new exmp.commands.Utils();
 
+    /**
+     * Возвращает название команды add.
+     *
+     * @return название команды add.
+     */
     @Override
     public String getName() {
         return "add";
     }
 
+    /**
+     * Возвращает описание команды add.
+     *
+     * @return описание команды add.
+     */
     @Override
     public String getDescription() {
         return "Добавить новый элемент в коллекцию";
     }
 
+    /**
+     * Выполняет команду добавления
+     *
+     * @param app объект приложения, над которым выполняется команда.
+     * @param args массив аргументов команды.
+     */
     @Override
     public void execute(exmp.App app, String[] args) {
         Product product = utils.ScanNewProduct();
