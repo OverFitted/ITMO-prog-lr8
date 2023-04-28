dff8ccc2a2cab27810f17fbfa21827ab74073f4e
OverFitted <kozak.bor@gmail.com>
2023-04-28 15:26:21 +0300

    Fix: packet sender fixed

--- integration-tests/src/test/java/exmp/ClientServerIntegrationTest.java
+++ integration-tests/src/test/java/exmp/ClientServerIntegrationTest.java
@@ -1,178 +1,87 @@
 package exmp;
 
 import exmp.commands.CommandResult;
-import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 
-import java.io.ByteArrayInputStream;
-import java.io.ByteArrayOutputStream;
-import java.io.ObjectInputStream;
-import java.io.ObjectOutputStream;
+import java.io.*;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.InetSocketAddress;
-import java.util.Random;
+import java.nio.ByteBuffer;
 
 import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.Mockito.doReturn;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 public class ClientServerIntegrationTest {
-    private final Random random = new Random();
-    private static final String TEST_HOST = "localhost";
-    private static exmp.App app;
-
-    int port;
-    Thread serverThread;
-
+    private static final int SERVER_PORT = 12321;
+    private static final String HOST = "localhost";
+    private static exmp.Server server;
+    private static final exmp.App app = new exmp.App("src/main/resources/input.xml");
 
     private static final Logger logger = LogManager.getLogger(ClientServerIntegrationTest.class);
 
     @BeforeAll
-    public static void setup() {
-        app = Mockito.spy(new exmp.App("src/main/resources/input.xml"));
+    public static void setUp() {
+        server = new exmp.Server(SERVER_PORT, app);
+        exmp.Client client = new exmp.Client(HOST, SERVER_PORT);
 
-        logger.info("Staring tests");
-    }
-
-    @AfterAll
-    public static void tearDown() {
-        app.switchOff();
-
-        logger.info("Testing finished");
-    }
-
-    @BeforeEach
-    public void serverSetup() {
-        port = random.nextInt(65535);
-        exmp.Server server = new exmp.Server(port, app);
-
-        when(app.getState()).thenReturn(true);
-
-        serverThread = new Thread(server::start);
-        serverThread.start();
+        new Thread(() -> server.start()).start();
     }
 
     @Test
-    public void testPacketLoss() throws Exception {
-        logger.debug("Packet loss test started");
-
-        String commandName = "add";
-        String commandInput = "Sample Product";
-        CommandResult result = sendCommandToServer(port + 1, commandName, commandInput);
-
-        assertNull(result);
-
-        serverThread.join();
-    }
-
-    @Test
-    public void testPacketDelay() throws Exception {
-        logger.debug("Packet delay test started");
-
-        String commandName = "add";
-        String commandInput = "Sample Product";
-        CommandResult expectedResult = new CommandResult(0, "Product added successfully.", null);
-        doReturn(expectedResult).when(app).executeCommand(commandName, commandInput);
-
-        CommandResult actualResult = sendCommandToServerWithDelay(port, commandName, commandInput, delay);
-
-        assertEquals(expectedResult.getStatusCode(), actualResult.getStatusCode());
-        assertEquals(expectedResult.getOutput(), actualResult.getOutput());
-
-        serverThread.join();
-    }
-
-    @Test
-    public void testIncorrectPacketOrder() throws Exception {
-        logger.debug("Packet incorrect order test started");
-
-        String commandName1 = "add";
-        String commandInput1 = "Sample Product 1";
-        CommandResult expectedResult1 = new CommandResult(0, "Product 1 added successfully.", null);
-        doReturn(expectedResult1).when(app).executeCommand(commandName1, commandInput1);
-
-        String commandName2 = "add";
-        String commandInput2 = "Sample Product 2";
-        CommandResult expectedResult2 = new CommandResult(0, "Product 2 added successfully.", null);
-        doReturn(expectedResult2).when(app).executeCommand(commandName2, commandInput2);
-
-        CommandResult actualResult2 = sendCommandToServer(port, commandName2, commandInput2);
-        CommandResult actualResult1 = sendCommandToServer(port, commandName1, commandInput1);
-
-        assertEquals(expectedResult1.getStatusCode(), actualResult1.getStatusCode());
-        assertNotEquals(expectedResult1.getOutput(), actualResult1.getOutput());
-
-        assertEquals(expectedResult2.getStatusCode(), actualResult2.getStatusCode());
-        assertNotEquals(expectedResult2.getOutput(), actualResult2.getOutput());
-
-        serverThread.join();
-    }
-
-    @Test
-    public void testClientServerCommunication() throws Exception {
-        logger.debug("Communication test started");
-
-        String commandName = "info";
+    public void testSuccessfulCommunication() throws IOException, ClassNotFoundException {
+        String commandName = "help";
         String commandInput = "";
 
-        CommandResult expectedResult = new CommandResult(0, null, null);
+        CommandResult expectedResult = app.executeCommand(commandName, commandInput);
+        CommandResult result = sendCommand(commandName, commandInput);
 
-        doReturn(expectedResult).when(app).executeCommand(commandName, commandInput);
-
-        CommandResult actualResult = sendCommandToServer(port, commandName, commandInput);
-
-        assertEquals(expectedResult.getStatusCode(), actualResult.getStatusCode());
-        assertEquals(expectedResult.getOutput(), actualResult.getOutput());
+        assertEquals(expectedResult.getStatusCode(), result.getStatusCode());
+        assertEquals(expectedResult.getOutput(), result.getOutput());
     }
 
-    private CommandResult sendCommandToServerWithDelay(int port, String commandName, String commandInput, int delayMillis) throws Exception {
-        try (DatagramSocket socket = new DatagramSocket()) {
-            ByteArrayOutputStream byteOutput = new ByteArrayOutputStream();
-            ObjectOutputStream objectOutput = new ObjectOutputStream(byteOutput);
-            objectOutput.writeObject(new exmp.commands.CommandData(commandName, commandInput));
-            byte[] dataToSend = byteOutput.toByteArray();
+    @Test
+    public void testPacketLoss() throws IOException, InterruptedException {
+        DatagramSocket mockedSocket = mock(DatagramSocket.class);
+        doNothing().when(mockedSocket).send(Mockito.any(DatagramPacket.class));
 
-            DatagramPacket packetToSend = new DatagramPacket(dataToSend, dataToSend.length, new InetSocketAddress(ClientServerIntegrationTest.TEST_HOST, port));
-            socket.send(packetToSend);
-
-            if (delayMillis > 0) {
-                Thread.sleep(delayMillis);
-            }
-
-            byte[] dataToReceive = new byte[4096];
-            DatagramPacket packetToReceive = new DatagramPacket(dataToReceive, dataToReceive.length);
-            socket.receive(packetToReceive);
-
-            ByteArrayInputStream byteInput = new ByteArrayInputStream(dataToReceive);
-            ObjectInputStream objectInput = new ObjectInputStream(byteInput);
-            return (CommandResult) objectInput.readObject();
-        }
     }
 
-    private CommandResult sendCommandToServer(int port, String commandName, String commandInput) throws Exception {
-        try (DatagramSocket socket = new DatagramSocket()) {
-            ByteArrayOutputStream byteOutput = new ByteArrayOutputStream();
-            ObjectOutputStream objectOutput = new ObjectOutputStream(byteOutput);
-            objectOutput.writeObject(new exmp.commands.CommandData(commandName, commandInput));
-            byte[] dataToSend = byteOutput.toByteArray();
+    @Test
+    public void testPacketDuplication() throws IOException, InterruptedException {
+    }
 
-            DatagramPacket packetToSend = new DatagramPacket(dataToSend, dataToSend.length, new InetSocketAddress(ClientServerIntegrationTest.TEST_HOST, port));
-            socket.send(packetToSend);
+    @Test
+    public void testPacketReordering() throws IOException, InterruptedException {
+    }
 
-            byte[] dataToReceive = new byte[4096];
-            DatagramPacket packetToReceive = new DatagramPacket(dataToReceive, dataToReceive.length);
-            socket.receive(packetToReceive);
+    private CommandResult sendCommand(String commandName, String commandInput) throws IOException, ClassNotFoundException {
+        try (DatagramSocket datagramSocket = new DatagramSocket()) {
+            exmp.commands.CommandData outCommand = new exmp.commands.CommandData(commandName, commandInput);
 
-            ByteArrayInputStream byteInput = new ByteArrayInputStream(dataToReceive);
-            ObjectInputStream objectInput = new ObjectInputStream(byteInput);
-            return (CommandResult) objectInput.readObject();
+            ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+            ObjectOutputStream objectOutputStream = new ObjectOutputStream(byteArrayOutputStream);
+            objectOutputStream.writeObject(outCommand);
+            byte[] data = byteArrayOutputStream.toByteArray();
+
+            DatagramPacket sendPacket = new DatagramPacket(data, data.length, new InetSocketAddress(HOST, SERVER_PORT));
+            datagramSocket.send(sendPacket);
+
+            ByteBuffer buffer = ByteBuffer.allocate(65536);
+            DatagramPacket receivePacket = new DatagramPacket(buffer.array(), buffer.capacity());
+            datagramSocket.receive(receivePacket);
+            buffer.position(receivePacket.getLength());
+
+            buffer.flip();
+            ByteArrayInputStream byteArrayInputStream = new ByteArrayInputStream(buffer.array(), 0, buffer.limit());
+            ObjectInputStream objectInputStream = new ObjectInputStream(byteArrayInputStream);
+
+            return (CommandResult) objectInputStream.readObject();
         }
     }
 }
