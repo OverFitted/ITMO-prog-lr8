f3a73db3e0861a427cd12e103a61c2deb75142c8
OverFitted <kozak.bor@gmail.com>
2023-04-28 15:53:55 +0300

    Feat: add some tests here and there

--- integration-tests/src/test/java/exmp/ClientServerIntegrationTest.java
+++ integration-tests/src/test/java/exmp/ClientServerIntegrationTest.java
@@ -1,24 +1,24 @@
 package exmp;
 
 import exmp.commands.CommandResult;
+import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
-import org.mockito.Mockito;
 
 import java.io.*;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.InetSocketAddress;
 import java.nio.ByteBuffer;
+import java.util.Random;
 
 import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.Mockito.*;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 public class ClientServerIntegrationTest {
-    private static final int SERVER_PORT = 12321;
+    private static final int SERVER_PORT = 52333;
     private static final String HOST = "localhost";
     private static exmp.Server server;
     private static final exmp.App app = new exmp.App("src/main/resources/input.xml");
@@ -26,15 +26,22 @@ public class ClientServerIntegrationTest {
     private static final Logger logger = LogManager.getLogger(ClientServerIntegrationTest.class);
 
     @BeforeAll
-    public static void setUp() {
-        server = new exmp.Server(SERVER_PORT, app);
-        exmp.Client client = new exmp.Client(HOST, SERVER_PORT);
+    public static void setup() {
+        logger.info("Starting tests...");
 
+        server = new exmp.Server(SERVER_PORT, app);
         new Thread(() -> server.start()).start();
+    }
+
+    @AfterAll
+    public static void join() {
+        logger.info("All tests finished");
     }
 
     @Test
     public void testSuccessfulCommunication() throws IOException, ClassNotFoundException {
+        logger.debug("Testing client-server communication...");
+
         String commandName = "help";
         String commandInput = "";
 
@@ -46,18 +53,62 @@ public class ClientServerIntegrationTest {
     }
 
     @Test
-    public void testPacketLoss() throws IOException, InterruptedException {
-        DatagramSocket mockedSocket = mock(DatagramSocket.class);
-        doNothing().when(mockedSocket).send(Mockito.any(DatagramPacket.class));
+    public void testPacketLoss() throws IOException, ClassNotFoundException {
+        String commandName = "help";
+        String commandInput = "";
 
+        CommandResult expectedResult = app.executeCommand(commandName, commandInput);
+
+        // Вероятность потери пакета (от 0 до 1, где 0 - нет потерь, 1 - все пакеты потеряны)
+        double packetLossProbability = 0.3;
+        Random random = new Random();
+
+        CommandResult result = null;
+        for (int i = 0; i < 5; i++) {
+            if (random.nextDouble() >= packetLossProbability) {
+                result = sendCommand(commandName, commandInput);
+            }
+
+            if (result != null && result.getStatusCode() == expectedResult.getStatusCode()) {
+                break;
+            }
+        }
+
+        assertNotNull(result, "Не удалось получить результат из-за потери пакетов");
+        assertEquals(expectedResult.getStatusCode(), result.getStatusCode());
+        assertEquals(expectedResult.getOutput(), result.getOutput());
     }
 
     @Test
-    public void testPacketDuplication() throws IOException, InterruptedException {
+    public void testPacketDuplication() throws IOException, ClassNotFoundException {
+        String commandName = "help";
+        String commandInput = "";
+
+        sendCommand(commandName, commandInput);
+        CommandResult result = sendCommand(commandName, commandInput);
+
+        CommandResult expectedResult = app.executeCommand(commandName, commandInput);
+
+        assertEquals(expectedResult.getStatusCode(), result.getStatusCode());
+        assertEquals(expectedResult.getOutput(), result.getOutput());
     }
 
     @Test
-    public void testPacketReordering() throws IOException, InterruptedException {
+    public void testPacketReordering() throws IOException, ClassNotFoundException {
+        String commandName1 = "info";
+        String commandInput1 = "";
+
+        String commandName2 = "help";
+        String commandInput2 = "";
+
+        CommandResult result2 = sendCommand(commandName1, commandInput1);
+        CommandResult result1 = sendCommand(commandName2, commandInput2);
+
+        CommandResult expectedResult1 = app.executeCommand(commandName1, commandInput1);
+        CommandResult expectedResult2 = app.executeCommand(commandName2, commandInput2);
+
+        assertNotEquals(expectedResult1.getOutput(), result1.getOutput());
+        assertNotEquals(expectedResult2.getOutput(), result2.getOutput());
     }
 
     private CommandResult sendCommand(String commandName, String commandInput) throws IOException, ClassNotFoundException {
