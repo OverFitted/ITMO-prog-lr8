9a1a12438164b7ec8ea2334c6a706bbcbaf7537b
OverFitted <kozak.bor@gmail.com>
2023-05-05 16:02:20 +0300

    Feat: add starter script

---
+++ starter.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+./gradlew build
+
+# Start the first three JAR files with nohup and logging
+nohup java -jar GatewayLBService/build/libs/GatewayLBService-1.0-SNAPSHOT-all.jar > gateway.log &
+nohup java -jar server/build/libs/server-1.0-SNAPSHOT-all.jar > server1.log &
+nohup java -jar server/build/libs/server-1.0-SNAPSHOT-all.jar > server2.log &
+nohup java -jar server/build/libs/server-1.0-SNAPSHOT-all.jar > server3.log &
+
+# Start the fourth JAR file with system.in
+java -jar client/build/libs/client-1.0-SNAPSHOT-all.jar
