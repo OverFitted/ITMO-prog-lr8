b86ed8b62b36d85ace2ae26eabd970b1d68c433b
OverFitted <kozak.bor@gmail.com>
2023-05-18 12:40:39 +0300

    Feat: add hikari pool, add db creds env parsing

--- internal/src/main/java/exmp/database/DatabaseConnection.java
+++ internal/src/main/java/exmp/database/DatabaseConnection.java
@@ -1,23 +1,47 @@
 package exmp.database;
 
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
+import io.github.cdimascio.dotenv.Dotenv;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
-import java.sql.*;
+import java.sql.Connection;
+import java.sql.SQLException;
 
 public class DatabaseConnection {
-    private static final String URL = "jdbc:postgresql://localhost:5432/studs";
-    private static final String USER = "postgres";
-    private static final String PASSWORD = "postgres";
-    private static final Logger logger = LogManager.getLogger(exmp.database.DatabaseConnection.class);
+    private static final Logger logger = LogManager.getLogger(DatabaseConnection.class);
+    private static final HikariDataSource ds;
+
+    static {
+        Dotenv dotenv = Dotenv.load();
+
+        String URL = "jdbc:postgresql://" + dotenv.get("HOST") + "/" + dotenv.get("TABLE");
+        String USER = dotenv.get("LOGIN");
+        String PASSWORD = dotenv.get("PASSWORD");
+
+        HikariConfig config = new HikariConfig();
+        config.setJdbcUrl(URL);
+        config.setUsername(USER);
+        config.setPassword(PASSWORD);
+
+        config.addDataSourceProperty("cachePrepStmts", "true");
+        config.addDataSourceProperty("prepStmtCacheSize", "250");
+        config.addDataSourceProperty("prepStmtCacheSqlLimit", "2048");
+
+        ds = new HikariDataSource(config);
+    }
 
     public static Connection getConnection() {
-        Connection connection = null;
         try {
-            connection = DriverManager.getConnection(URL, USER, PASSWORD);
+            return ds.getConnection();
         } catch (SQLException e) {
             logger.error("Ошибка подключения к базе данных: " + e);
+            return null;
         }
-        return connection;
+    }
+
+    public static void closeDataSource() {
+        ds.close();
     }
 }
